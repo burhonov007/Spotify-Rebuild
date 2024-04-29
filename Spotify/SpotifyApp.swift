226ef993553e276ad54d5c71b28da2b6a7a518b1
Akmalkhon Burkhonov <akmalburhonov5515@gmail.com>
2024-04-29 09:43:21 +0500

    Initial Commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Akmalkhon on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
